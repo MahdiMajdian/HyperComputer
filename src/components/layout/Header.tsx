0bc8ae703f08fc41eec9cde9a866686e1e40c1f5
Mahdi Majdian <mahdi2011.shz@gmail.com>
2021-08-24 13:38:25 +0430

    Header components created and added to the Layout

---
+++ src/components/layout/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <header className='w-full h-48 bg-blue-400'>
+            Test
+        </header>
+    )
+}
+
+export default Header
--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,10 +1,12 @@
 import React from "react"
+import Header from "./Header"
 
 interface IHeader {}
 
 const Layout: React.FC<IHeader> = (props) => {
 	return (
 		<>
+			<Header />
 			<main>{props.children}</main>
 		</>
 	)
