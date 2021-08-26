fac1399bfc7300d7f86406a99f443279422964d3
Mahdi Majdian <mahdi2011.shz@gmail.com>
2021-08-26 13:26:50 +0430

    dark mode feature added and tailwind configured

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,50 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import "./App.css"
 import Layout from "./components/layout/Layout"
 
 function App() {
+	const [darkMode, setdarkMode] = useState<string>(
+		localStorage.theme || "light"
+	)
+	if (darkMode === "dark") {
+		document.documentElement.classList.add("dark")
+	} else {
+		document.documentElement.classList.remove("dark")
+	}
+	useEffect(() => {
+		window
+			.matchMedia("(prefers-color-scheme: dark)")
+			.addEventListener("change", (event) => {
+				console.log("run")
+				// if (!("theme" in localStorage)) {
+				if (event.matches) {
+					setdarkMode("dark")
+				} else {
+					setdarkMode("light")
+				}
+				// }
+			})
+	}, [])
+
+	const toggle = () => {
+		if (darkMode === "dark") {
+			localStorage.theme = "light"
+			setdarkMode("light")
+		} else {
+			localStorage.theme = "dark"
+			setdarkMode("dark")
+		}
+	}
+
 	return (
 		<Layout>
-			<div className="font-semibold text-5xl text-center">
-				Hello World
+			<div className="font-semibold text-xl text-center">
+				<p>Toggle Dark Mode:</p>
+				<button
+					className="bg-green-500 shadow m-4 rounded p-2 text-white"
+					onClick={toggle}>
+					Dark mode
+				</button>
 			</div>
 		</Layout>
 	)
--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from "react"
 
 const Header = () => {
-    return (
-        <header className='w-full h-48 bg-blue-400'>
-            Test
-        </header>
-    )
+	return (
+		<header className="w-full h-48 bg-gray-200 dark:bg-gray-700">
+			<p className='text-center font-medium text-3xl text-black dark:text-white pt-8'>Header to be constructed...</p>
+		</header>
+	)
 }
 
 export default Header
