3e3c5788aca4cea66255031f8ec7ef169c002f25
Mahdi Majdian <mahdi2011.shz@gmail.com>
2021-08-27 11:49:08 +0430

    submenu for header created

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,17 +1,24 @@
 import React from "react"
-import { FiShoppingCart, FiUser, FiSearch } from "react-icons/fi"
+import {
+	FiShoppingCart,
+	FiUser,
+	FiSearch,
+	FiMoreHorizontal,
+	FiChevronDown,
+} from "react-icons/fi"
+import { BiCoin } from "react-icons/bi"
 import logo from "../../assets/images/logo.webp"
 const Header = () => {
 	return (
-		<header className="w-screen h-20 flex justify-center bg-white dark:bg-gray-800 fixed">
-			<div className="h-full w-full flex justify-around items-center mx-auto max-w-7xl text-gray-800 dark:text-white">
+		<header className="w-screen flex flex-col justify-center bg-white dark:bg-gray-800 fixed shadow-md text-gray-800 dark:text-white">
+			<div className="h-20 w-full flex justify-around items-center mx-auto max-w-7xl ">
 				<div>
 					<img src={logo} alt="logo" className="h-10" />
 				</div>
 
-				<div className="w-96 flex items-center transition ease-in-out duration-500 hover:bg-gray-300 bg-gray-200 dark:bg-gray-600 dark:hover:bg-gray-700  rounded-full">
+				<div className="w-96 flex items-center transition ease-in-out duration-300 hover:bg-gray-300 bg-gray-200 dark:bg-gray-600 dark:hover:bg-gray-700  rounded-full">
 					<input
-						className="w-full  text-sm bg-transparent transition ease-in-out duration-500 bg-gray-200 dark:bg-gray-600 hover:bg-gray-200 dark:hover:bg-gray-600 rounded-full dark:placeholder-white dark:text-white outline-none p-2.5"
+						className="w-full text-sm bg-transparent transition ease-in-out duration-300 bg-gray-200 dark:bg-gray-600 hover:bg-gray-200 dark:hover:bg-gray-600 rounded-full  dark:text-white outline-none p-2.5"
 						placeholder="دنبال چه چیزی می گردید؟"
 					/>
 					<a href="#" className="p-2.5">
@@ -21,10 +28,10 @@ const Header = () => {
 
 				<div className="flex items-center gap-4">
 					<div className="">
-						<p className="text-gray-500 text-xs py-1">
+						<p className="text-gray-500 text-xs py-0.5">
 							با ما تماس بگیرید
 						</p>
-						<p className="text-sm py-1">021-87700265</p>
+						<p className="text-sm py-0.5">021-87700265</p>
 					</div>
 					<div className="flex ">
 						<button className="relative">
@@ -37,6 +44,68 @@ const Header = () => {
 					<button>
 						<FiUser className="btn drop-shadow-lg" />
 					</button>
+				</div>
+			</div>
+			{/* subHeader */}
+			<div className="h-10 w-full text-sm text-gray-600 font-medium dark:text-gray-200 flex justify-around items-center mx-auto max-w-7xl ">
+				{/* right category */}
+				<div>
+					<ul className="flex gap-4">
+						<li>
+							<a href="#" className="flex items-center gap-1">
+								<p>دسته بندی ها</p>
+								<FiChevronDown className="text-green-400" />
+							</a>
+						</li>
+						<li>
+							<a href="#">
+								<p>چرا هایپر کامپیوتر</p>
+							</a>
+						</li>
+						<li>
+							<a href="#">
+								<p>راهنمای خرید</p>
+							</a>
+						</li>
+						<li>
+							<a href="#">
+								<p>هارد پی‌سی</p>
+							</a>
+						</li>
+						<li>
+							<a href="#">
+								<p>شرایط گارانتی</p>
+							</a>
+						</li>
+						<li>
+							<a href="#" className="flex items-center gap-1">
+								<p>سایر</p>
+								<FiMoreHorizontal className="text-green-400" />
+							</a>
+						</li>
+					</ul>
+				</div>
+				<div>
+					<ul className="flex gap-4">
+						<li>
+							<a href="#" className="flex items-center gap-2">
+								<BiCoin className="text-green-500 text-base" />
+								<p>خرید اقساطی</p>
+							</a>
+						</li>
+						<li>
+							<a href="#" className="flex items-center gap-2">
+								<BiCoin className="text-green-500 text-base" />
+								<p>فروش ویژه</p>
+							</a>
+						</li>
+						<li>
+							<a href="#" className="flex items-center gap-2">
+								<BiCoin className="text-green-500 text-base" />
+								<p>دست دوم</p>
+							</a>
+						</li>
+					</ul>
 				</div>
 			</div>
 		</header>
