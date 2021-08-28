966372d61846ac0dbb21d3b983d05b3cd750970e
Mahdi Majdian <mahdi2011.shz@gmail.com>
2021-08-28 20:36:07 +0430

    Footer component created and added to Layout wrapper

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,4 +1,5 @@
 import React from "react"
+import Footer from "./Footer"
 import Header from "./Header"
 
 interface ILayout {
@@ -10,6 +11,7 @@ const Layout: React.FC<ILayout> = (props) => {
 		<>
 			<Header themeToggle={props.themeToggle} />
 			<main>{props.children}</main>
+			<Footer />
 		</>
 	)
 }
