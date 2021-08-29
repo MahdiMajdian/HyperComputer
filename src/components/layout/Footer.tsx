08b4721282d6cc00a845ea88da4c86df12c65f61
Mahdi Majdian <mahdi2011.shz@gmail.com>
2021-08-29 20:48:37 +0430

    subscription section of the footer made responsive

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -8,10 +8,10 @@ import { FiFilm } from "react-icons/fi"
 import { FaTelegram, FaEnvelope } from "react-icons/fa"
 const Footer = () => {
 	return (
-		<footer className="relative bg-white dark:bg-gray-800 w-full h-96">
-			<div className="mx-auto inset-x-0 w-full max-w-7xl p-10 textw flex justify-between absolute -top-10 bg-white dark:bg-gray-800 dark:text-white rounded-lg shadow-lg">
-				<p className="my-auto">عضویت در خبرنامه</p>
-				<div className="flex rounded-full bg-green-100 dark:bg-gray-700 w-full h-12 max-w-lg px-4">
+		<footer className="relative transition duration-300 bg-white dark:bg-gray-800 w-full h-96">
+			<div className="mx-auto inset-x-0 w-full max-w-7xl p-6 flex flex-col items-center justify-center md:flex-row  md:justify-between gap-4 absolute -top-10 transition duration-300 bg-white dark:bg-gray-800 dark:text-white rounded-lg shadow-lg">
+				<p className=" ">عضویت در خبرنامه</p>
+				<div className="flex rounded-full lg:w-full mx-auto transition duration-300 bg-green-100 dark:bg-gray-700  h-12 max-w-md px-4">
 					<input
 						className="w-full bg-transparent outline-none placeholder-gray-500 "
 						placeholder="ایمیل خود را وارد کنید"
@@ -20,8 +20,8 @@ const Footer = () => {
 						<FaEnvelope className="w-6 h-auto ml-4 mr-1 text-green-400" />
 					</button>
 				</div>
-				<div>
-					<ul className="flex gap-4">
+				
+					<ul className="flex gap-4 mx-auto">
 						<li className="rounded-xl bg-pink-100 w-10   h-14 flex items-center p-2 text-pink-600">
 							<FiFilm className="w-full h-auto" />
 						</li>
@@ -38,7 +38,7 @@ const Footer = () => {
 							<AiFillYoutube className="w-full h-auto" />
 						</li>
 					</ul>
-				</div>
+				
 			</div>
 		</footer>
 	)
