3f13ffc246ed49c1f3b0125b48db9d345f0af820
Mahdi Majdian <mahdi2011.shz@gmail.com>
2021-09-02 22:58:36 +0430

    redux toolkit configured on project

---
+++ src/store/store.ts
@@ -0,0 +1,14 @@
+import { configureStore } from '@reduxjs/toolkit'
+import productsSlice from './products-slice'
+
+export const store = configureStore({
+  reducer: {
+    products: productsSlice.reducer 
+  },
+})
+
+// Infer the `RootState` and `AppDispatch` types from the store itself
+export type RootState = ReturnType<typeof store.getState>
+// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+export type AppDispatch = typeof store.dispatch
+export default store
